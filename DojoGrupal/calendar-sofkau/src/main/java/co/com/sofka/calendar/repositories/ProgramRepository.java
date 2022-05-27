62c073d069764cc12891c03cac95d37f301c3712
anameb <anameb92@gmail.com>
2022-05-27 17:09:04 -0500

    Se clona y crea repositorio de Dojo

--- DojoGrupal/calendar-sofkau/src/main/java/co/com/sofka/calendar/repositories/ProgramRepository.java
+++
@@ -1,8 +0,0 @@
-package co.com.sofka.calendar.repositories;
-
-import co.com.sofka.calendar.collections.Program;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface ProgramRepository extends ReactiveMongoRepository<Program, String> {
-
-}
